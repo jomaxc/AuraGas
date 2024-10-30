d38efc14f2c7a8185216b17a55bfd6f7e677b9a0
jomaxc <authorjomax@gmail.com>
2024-10-30 11:18:38 -0600

    Projectile Gameplay Effect

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -7,7 +7,7 @@
 #include "AuraProjectileSpell.generated.h"
 
 class AAuraProjectile;
-
+class UGameplayEffect;
 /**
  * 
  */
@@ -25,4 +25,7 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
 };
