a3c3a5eebca2f7d31d5d6e1fb0d3f697ea2ce3e1
jomaxc <authorjomax@gmail.com>
2025-01-02 18:00:29 -0700

    Attribute Points Member Variable

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -4,6 +4,7 @@
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
+#include "Player/AuraPlayerState.h"
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
@@ -18,6 +19,13 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 		}
 	);
 	}
+	AAuraPlayerState* AuraPlayerState = CastChecked<AAuraPlayerState>(PlayerState);
+	AuraPlayerState->OnAttributePointsChangedDelegate.AddLambda(
+		[this](int32 Points)
+		{
+			AttributePointsChangedDelegate.Broadcast(Points);
+		}
+	);
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
